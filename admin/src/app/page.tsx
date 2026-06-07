e428c2ef5d334068ff6737e2445150adf1f95d46
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2026-06-07 21:22:15 +0300

    add dashboard charts

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,6 +1,16 @@
-import { Button } from "@/components/ui/button"
-import { CirclePlus } from "lucide-react"
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 
 export default function HomePage() {
-  return <div className="flex h-screen items-center justify-center"></div>
+  return <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+      <AppBarChart/>
+    </div>
+    <div className="bg-primary-foreground p-4 rounded-lg"></div>
+    <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart/></div>
+    <div className="bg-primary-foreground p-4 rounded-lg"></div>
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppAreaChart/></div>
+    <div className="bg-primary-foreground p-4 rounded-lg"></div>
+  </div>
 }
